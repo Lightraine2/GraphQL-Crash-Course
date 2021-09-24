bc1824c77404e4cf526a28b06dbcc7d8c6a516cd
Lightraine2 <laurencedale1986@gmail.com>
2021-09-24 21:58:24 +0100

    to 48

--- start/server/index.js
+++ start/server/index.js
@@ -1,1 +1,63 @@
-const {ApolloServer, gql} = reqi
+const {ApolloServer, gql} = require('apollo-server');
+
+const typeDefs = gql `
+
+type Book {
+    title: String
+    author: String
+    }
+
+type MainCard {
+    title: String
+    image: String
+    }    
+
+type Query {
+    books: [Book]
+    mainCards: [MainCard]
+    }
+
+`;
+
+const books = [
+    {
+        title: 'The Innocent Wife',
+        author: 'David Balladaci'
+    },
+    {
+        title: 'The Fountainhead',
+        author: 'Ayn Rand'
+    },
+    {
+        title: 'Atlas Shrugged',
+        author: 'Ayn Rand'
+    },
+]
+const mainCards = [
+    {
+        title: 'Recently Viewed',
+        image: 'lion'
+    },
+    {
+        title: 'Looking for a Gift?',
+        image: 'penguin'
+    },
+    {
+        title: 'Best Behaved',
+        image: 'Cat'
+    },
+]
+
+const resolvers = {
+    Query: {
+        books: () => books,
+        mainCards: () => mainCards
+    },
+};
+
+
+const server = new ApolloServer({ typeDefs, resolvers});
+
+server.listen().then(({url}) => {
+    console.log(`Server started at ${url}`)
+});
